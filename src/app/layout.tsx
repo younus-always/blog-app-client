7e9e4307dbbaceba103cae26bd4effec285e8a8b
younus-always <younusofficial443@gmail.com>
2026-08-20 21:10:02 +0600

    layout: created route-group layout folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/layout/Navbar";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const geistMono = Geist_Mono({
@@ -24,7 +23,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
