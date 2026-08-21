b5263670eb29b2d3c31dc9ec4cceb1b5fe456562
younus-always <younusofficial443@gmail.com>
2026-08-21 09:21:03 +0600

    login registration page and better-auth installation

--- src/app/(commonLayout)/register/page.tsx
+++ src/app/(commonLayout)/register/page.tsx
@@ -1,6 +1,11 @@
+import { RegisterForm } from "@/components/modules/authentication/register-form"
 
-export default function RegisterPage() {
-      return (
-            <div>RegisterPage</div>
-      )
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <RegisterForm />
+      </div>
+    </div>
+  )
 }
