7b47b50d38b3be71bd8ef87c309c290ad8be2e57
younus-always <younusofficial443@gmail.com>
2026-08-21 12:08:39 +0600

    feat: Authentication with google oauth and getSession data in server component using fetch

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,5 +1,15 @@
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const res = await fetch("http://localhost:5000/api/auth/get-session", {
+    headers: {
+      Cookie: cookieStore.toString()
+    },
+    cache: "no-store"
+  });
+  const session = await res.json();
+
   return (
     <div>
       <h1>Home page</h1>
