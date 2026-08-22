53ecc7671ac1f85b66a95d6560a9c97826da5d17
younus-always <younusofficial443@gmail.com>
2026-08-22 22:22:12 +0600

    modify: user login and registration successful with tanstack form

--- src/components/modules/authentication/register-form.tsx
+++ src/components/modules/authentication/register-form.tsx
@@ -9,9 +9,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Field, FieldDescription, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { useForm } from "@tanstack/react-form";
+import Link from "next/link";
+import { toast } from "sonner";
 import z from "zod";
 
 
@@ -33,10 +36,36 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
       onSubmit: formSchema,
     },
     onSubmit: async ({ value }) => {
-      console.log(value);
+      const toastId = toast.loading("Creating user");
+      try {
+        const { data, error } = await authClient.signUp.email(value);
+
+        if (error) {
+          toast.error(error.message, { id: toastId })
+          return
+        };
+
+        toast.success("User Created Successfully", { id: toastId });
+      } catch (err) {
+        toast.error("Something went wrong. Please try again!", { id: toastId });
+      }
     }
   });
 
+  const handleGoogleLogin = async () => {
+    try {
+      const { data, error } = await authClient.signIn.social({
+        provider: "google",
+        callbackURL: "http://localhost:3000",
+      });
+
+      if (error) {
+        console.log("Google login error:", error);
+      };
+    } catch (error) {
+      console.error("Google login failed:", error);
+    }
+  };
 
   return (
     <Card {...props}>
@@ -123,8 +152,25 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
           </FieldGroup>
         </form>
       </CardContent>
-      <CardFooter className="flex justify-end">
-        <Button form="register-form" type="submit">Submit</Button>
+      <CardFooter className="flex flex-col gap-5">
+        <Button
+          type="submit"
+          form="register-form"
+          className="w-full"
+        >
+          Register
+        </Button>
+        <Button
+          onClick={() => handleGoogleLogin()}
+          variant="outline"
+          type="button"
+          className="w-full"
+        >
+          Continue with Google
+        </Button>
+        <FieldDescription className="text-center">
+          Already have an account? <Link href="/login">Login</Link>
+        </FieldDescription>
       </CardFooter>
     </Card>
   )
